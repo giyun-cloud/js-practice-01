bc05d9743a57ac21f09d93f61ef79d2d315e3c07
giyun-cloud <nangiyoon@gmail.com>
2021-04-02 20:32:03 +0900

    Ver2

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
-const ulEl = document.querySelector('ul')
-for (let i = 1; i < 11; i++) {
-  const li = document.createElement('li');
-  if(i < 9) {
-    li.textContent = `Warrior - ${i}`
-  }else if(i >= 9){
-    switch(i) {
-      case 9:
-        li.textContent = `Warrior - Commander`
-        li.addEventListener('click', function() {
-          console.log("저는 이 무리의 부대장입니다.")
-        })
-        break
-      case 10:
-        li.textContent = `Warrior - Leader`
-        li.addEventListener('click', function() {
-          console.log("저는 이 무리의 대장입니다.")
-        })
-        break
-    }
-  }
-  ulEl.appendChild(li)
-}
+const arrayZ = ['Goku','Vegeta','Picolo']
+arrayZ2 = arrayZ.forEach((el,i) => {
+  return `${el} - ${i}`
+});
+console.log(arrayZ2)
 
+arrayZ3 = arrayZ.map((el,i) => {
+  return `${el} - ${i}`
+});
+console.log(arrayZ3)
+
+arrayZ4 = arrayZ.map((el,i) => {
+  return {Name: el,  Rank: i}
+});
+
+console.log(arrayZ4)
