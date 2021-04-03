8327999ac9b9792f46a0a53461d99ea0de2d7f74
giyun-cloud <nangiyoon@gmail.com>
2021-04-03 12:27:56 +0900

    ver4

--- main.js
+++ main.js
@@ -1,121 +1,86 @@
-//화살표 함수
-const arw = 10
+//String: indexof, length, slice, replace, match, trim
+const str1 = 'JavaScript is fun'
+console.log(str1.indexOf('is'))
+console.log(str1.length)
+console.log(str1.slice(1,-10))
+console.log(str1.replace("JavaScript ",""))
+console.log(str1.match(/.+(?=is)/))
+const str2 = '  JavaScript is fun  '
+console.log(str2)
+console.log(str2.trim())
 
-const arw1 = function (arw) {
-  return arw+1
+//Number: toFixed, parseInt, parseFloat
+const num1 = 10.555555
+console.log(num1)
+console.log(num1.toFixed(3))
+console.log(typeof num1.toFixed(3))
+const num1Str = num1.toFixed(3)
+console.log(num1Str)
+console.log(parseInt(num1Str))
+console.log(parseFloat(num1Str))
+
+//Array: find, length, concat, forEach, map, filter, find,findIndex
+// includes, push, unshift, reverse, splice
+const array1 = ['Naruto','Saske','Gara','Rok-lee']
+const array1Find = array1.find((el,i) => el === 'Saske')
+console.log(array1Find)
+console.log(array1.length)
+const array2 = [3,7,1004,10000]
+console.log(array2.concat(array1))
+console.log(array1, array2)
+array2.forEach((el,i,infor) => {
+  console.log(el,`index : ${i}`,infor)
+})
+// array1Map = array1.map((el,i) => `No.${i+1} - ${el}`)
+array1Map = array1.map((el,i) => i >1)
+console.log(array1Map)
+array1Filter = array1.filter((el,i) => i >1)
+console.log(array1Filter)
+const array1FindIndex = array1.findIndex((el,i) => /^R/.test(el)) // R로시작하는거 찾기
+const rock = 'Rock'
+console.log(/^R/.test(rock))
+console.log(array1FindIndex)
+console.log(array2.includes(1004))
+console.log(array2.includes('1004'))
+array2.push(54)
+console.log(array2)
+array2.unshift(78)
+console.log(array2)
+array1.reverse()
+console.log(array1)
+const array1Splice = array1.splice(2,1,'ab')
+console.log(array1, array1Splice)
+
+//Object: 스태틱: assign, keys
+const obj1 = {
+  bronze: 10,
+  silver: 40,
+  gold: 80,
+  platinum: 100,
 }
-const arw2 = (arw) => {
-  return arw+2
+const obj1_ver2 = {
+  bronze: 10,
+  silver: 40,
+  gold: 80,
+  platinum: 95,
+  dia: 100
 }
-const arw3 = (arw) => arw+3
-const arw4 = arw => arw+4
+const obj1Assign2 = Object.assign({}, obj1,obj1_ver2)
+console.log(obj1Assign2, obj1)
 
-console.log(arw1(arw),arw2(arw),arw3(arw),arw4(arw));
+console.log(Object.assign(obj1,obj1_ver2),obj1)
+const obj1Assign = Object.assign(obj1,obj1_ver2)
+console.log(obj1Assign === obj1) // 같은 메모리주소를 가지고있음.
 
-//즉시실행함수
+const obj1KeysArray = Object.keys(obj1_ver2)
+console.log(obj1KeysArray)
+const obj1Values = obj1KeysArray.map((el) => obj1_ver2[el])
+console.log(obj1Values)
+//구조분해할당
 
-function ai() {
-  console.log(21)
-};
+const {silver, gold, dia: dia1, master = 100} = obj1_ver2
+console.log(silver, gold, dia1, master)
 
-(function () {
-  console.log(22)
-})();
-
-(function () {
-  console.log(23)
-}())
-
-
-
-//타이머함수
-const timeout = setTimeout(() => console.log('타이머함수'),1000)
-const interout = setInterval(() => console.log('interval타이머함수'), 1500);
-const randomT = Math.random()
-const timeRandom = randomT >= 0.5 ? 1000 : 999
-console.log(`타이머함수 랜덤 : ${randomT}`)
-setTimeout(() => clearTimeout(timeout), timeRandom)
-setTimeout(() => clearInterval(interout), 4500);
-
-
-
-//콜백
-const cb1 = function () {
-  console.log('녕')
-}
-function cb2(cb) {
-  console.log('안')
-  cb()
-}
-cb2(cb1)
-cb2(() => console.log('녕'))
-
-
-
-//생성자 함수,prototype
-//생성자 함수는 파스칼 케이스로 해야한다
-const Fighter = function (name,rank) {
-  this.name = name
-  this.rank = rank
-}
-
-const gohan = new Fighter('gohan',1)
-const goku = new Fighter('gohan',3)
-const majin_boo = new Fighter('boo',2)
-console.log(gohan)
-console.log(goku)
-console.log(majin_boo)
-
-Fighter.prototype.getPower = function () {
-  console.log(`${this.name}'s Power : ${100 * (101 - this.rank)}`)
-}
-gohan.getPower()
-majin_boo.getPower()
-goku.getPower()
-
-//화살표와 일반 함수의 this차이
-
-const vegeta = {
-  name: 'vegeta',
-  rank: 4,
-  getPower2() {
-    console.log(`${this.name}'s Power : ${80 * (101 - this.rank)}`)
-  },
-  getPower2Arrow() {setTimeout(() => console.log(`${this.name}'s Power : ${80 * (101 - this.rank)}`), 0)}
-  
-}
-
-console.log(vegeta)
-vegeta.getPower2()
-vegeta.getPower2Arrow()
-
-//class
-class Fighter3 {
-  constructor(name,rank){
-    this.name = name
-    this.rank = rank
-  }
-  getPower() {
-    console.log(`${this.name}'s Power : ${50 * (101 - this.rank)}`)
-  }
-}
-
-const picolo = new Fighter3('picolo',5)
-picolo.getPower()
-
-//class 상속
-class Fighter4 extends Fighter3{
-  constructor(name,rank,fusion){
-    super(name,rank)
-    this.fusion = fusion
-    this.rank = this.fusion ? this.rank -6 : this.rank
-    this.name = this.fusion ? 'gotenks' : name
-  }
-  getPower() {
-    console.log(`${this.name}'s Power : ${(this.fusion ? 100 : 50) * (101 - this.rank)}`)
-  }
-}
-// const goten = new Fighter4('goten',6,true)
-const goten = new Fighter4('goten',6,false)
-console.log(goten)
-goten.getPower();
+const array3 = ['Naruto','Saske','Gara','Rok-lee']
+const [a,,as,g,e = 'Gai'] = array3
+console.log(a,as,g,e)
